7d77b7f218e6007bfd2e88bd3143fd3eb9dbce63
Alex <Alex@MacBook-Pro.local>
2020-10-11 19:04:26 +0100

    Set database on Heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -133,9 +133,9 @@ LOGIN_REDIRECT_URL = '/'
 
 # Heroku: Update database configuration from $DATA_BASE_URL
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(default='postgres://...')
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

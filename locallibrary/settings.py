5e5526fa9601b9a9e1b0390a5d642744cf313fb1
Alex <Alex@MacBook-Pro.local>
2020-10-13 00:08:20 +0100

    Set default sender test

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = '116220522@umail.ucc.ie' 
